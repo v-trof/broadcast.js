44c2310c8eaf39910dccbaecad5b15c73a7310d0
Daniel Kuznetsov <afc2008@yandex.ru>
2016-09-24 17:39:48 +0300

    Router auto-test fixes
    * Closes #1

--- test/spec/routerSpec.js
+++ test/spec/routerSpec.js
@@ -2,26 +2,21 @@ describe('Router', function() {
   var router_message_recieved;
   beforeEach(function() {
     router_message_recieved = 0;
-  });
-
-  it('should route messages to the mediator', function(done) {
     broadcast.subscribe(test_name, function(value, message) {
       router_message_recieved = message;
-      done();
     });
+  });
 
+  it('should route messages to the mediator', function() {
     var message_sent = new Broadcast._src.Message('this one should be routed', channel._host, test_name);
     broadcast._router.parse_message(message_sent);
     expect(router_message_recieved).toEqual(message_sent);
   });
 
-  it('should keep the type of value while routing', function (done) {
-    broadcast.subscribe(test_name, function(value, message) {
-      router_message_recieved = message;
-      done();
-    });
+  it('should keep the type of value while routing', function () {
     var message_sent = new Broadcast._src.Message(new File([""], 'filename'), channel._host, test_name);
     broadcast._router.parse_message(message_sent);
     expect(router_message_recieved.value).toBe(message_sent.value);
   });
+
 });
